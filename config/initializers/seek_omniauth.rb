c6b2817d3e1404c8a228e57ec622e9727da1fb0a
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-02-14 14:22:48 +0000

    Switch config to use Rails 7 defaults.
    Use `after_initialize` in various initializers that rely on `Settings` model before it is loaded.

--- config/initializers/seek_omniauth.rb
+++ config/initializers/seek_omniauth.rb
@@ -1,21 +1,28 @@
 # use the rails logger for loggin OmniAuth; otherwise it will use std::out
 OmniAuth.config.logger = Rails.logger
-
-if Seek::Config.omniauth_enabled
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    # To add more providers, see the `omniauth_providers` definition in: `lib/seek/config.rb`
-    begin
-      providers = Seek::Config.omniauth_providers
-    rescue Settings::DecryptionError
-      providers = {}
-    end
-
-    providers.each do |key, options|
-      if options.is_a?(Array)
-        provider key, *options
-      else
-        provider key, options
+callme = -> {}
+Rails.configuration.after_initialize do
+  if Seek::Config.omniauth_enabled
+    callme = -> {
+      # To add more providers, see the `omniauth_providers` definition in: `lib/seek/config.rb`
+      begin
+        providers = Seek::Config.omniauth_providers
+      rescue Settings::DecryptionError
+        providers = {}
       end
-    end
+
+      providers.each do |key, options|
+        if options.is_a?(Array)
+          provider key, *options
+        else
+          provider key, options
+        end
+      end
+    }
   end
 end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  callme
+end
+
